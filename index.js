59d79291fc361e21346f712c6a66be17d0467218
SODOmize <121647062+SODOmize@users.noreply.github.com>
2020-02-11 15:17:42 +0000

    Missed a bit

---
+++ index.js
@@ -0,0 +1,5 @@
+
+var data = require('unicode-mandarin-readings')
+var surnameSubs = require('./surname-subs.json')
+var romanizationSys = require('./romanization-pairs.json')
+var removeDiacritics = require('diacritics').remove
