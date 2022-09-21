eefb4364d863b7b3f2d579dce52d8c98ec57a160
SODOmize <121647062+SODOmize@users.noreply.github.com>
2022-09-21 10:52:04 +0000

    Unit test fixes

--- test/test.js
+++ test/test.js
@@ -2,4 +2,5 @@ var romanize = require('../index.js')
 var test = require('tape')
 var systems = ['WG', 'MPS-II', 'TONGYONG', 'HANYU']
 
-test('2-char name', function (t) 
+test('2-char name', function (t) {
+  var re
