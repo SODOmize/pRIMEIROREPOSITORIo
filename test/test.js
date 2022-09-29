ecd58b95f3bba8ad76dc578e6d65197d9abede40
SODOmize <121647062+SODOmize@users.noreply.github.com>
2022-09-29 12:16:32 +0000

    Removed commented line

--- test/test.js
+++ test/test.js
@@ -3,4 +3,4 @@ var test = require('tape')
 var systems = ['WG', 'MPS-II', 'TONGYONG', 'HANYU']
 
 test('2-char name', function (t) {
-  var results = ['Hsuan Chang', 'Shiuan Jang', 'Syuan Jhang', 'X
+  var results = ['Hsuan Chang', 'Shiuan Jang', 'Syuan Jhang', 'Xuan Zh
