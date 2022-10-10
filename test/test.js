4de7df2d7a6ae571e4c2564058fa438b487256cc
SODOmize <121647062+SODOmize@users.noreply.github.com>
2022-10-10 06:56:30 +0000

    Change filename

--- test/test.js
+++ test/test.js
@@ -8,4 +8,6 @@ test('2-char name', function (t) {
   systems.forEach(function (sys, i) {
     var name = romanize('張懸', sys)
     t.equal(results[i], name, 'translated in ' + sys + ' correctly.')
-  
+  })
+
+  t.e
