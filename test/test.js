4f1ea876b7a61d18c36559a2242e946367bbfcb9
SODOmize <121647062+SODOmize@users.noreply.github.com>
2022-10-30 18:44:17 +0000

    Fixed build failure

--- test/test.js
+++ test/test.js
@@ -16,4 +16,5 @@ test('2-char name', function (t) {
 test('3-char name', function (t) {
   var results = ['Mu-An Chiu', 'Mu-An Chiou', 'Mu-An Ciou', 'Mu-An Qiu']
 
-  systems.forEach(function 
+  systems.forEach(function (sys, i) {
+    var
