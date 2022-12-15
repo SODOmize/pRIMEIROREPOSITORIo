20780c02c6c8883d312d52b8121dea23c840ec4d
SODOmize <121647062+SODOmize@users.noreply.github.com>
2022-12-15 20:58:13 +0000

    format code

--- test/test.js
+++ test/test.js
@@ -27,4 +27,5 @@ test('3-char name', function (t) {
 test('4-char name', function (t) {
   var results = ['Chun-Hung Fan-Chiang', 'Jiun-Hung Fan-Jiang', 'Jyun-Hong Fan-Jiang', 'Jun-Hong Fan-Jiang']
 
-  systems.forEach(function (sys, i)
+  systems.forEach(function (sys, i) {
+    var name 
